23d4c776af7a67a622aae25aa2bee51d054a8a26
muhimron90 <muh.imron90@gmail.com>
2020-10-14 13:11:06 +0700

    fix:puppeeter

--- src/index.js
+++ src/index.js
@@ -3,29 +3,29 @@ const https = require('https')
 const puppeteer = require('puppeteer-core')
 const path = require('path')
 
-const downloadFiles = (url, dest) =>
+const downloadFiles = (url, fileDest) =>
   new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(dest);
+    const file = fs.createWriteStream(fileDest);
 
     https
       .get(url, (response) => {
         response.pipe(file);
 
-        file.on('All Done !', () => {
+        file.on('finish', () => {
           file.close(resolve(true));
         });
       })
-      .on('Something Wrong', (err) => {
-        fs.unlink(dest);
+      .on('error', (error) => {
+        fs.unlink(fileDest);
 
-        reject(err.message);
+        reject(error.message);
       });
   });
 
 
  function RunNow() {
    (async () => {
-     const trims = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"; //path ur btowser
+     const trims = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
      const browser = await puppeteer.launch({
        headless: false,
        executablePath: trims, 
@@ -34,28 +34,25 @@ const downloadFiles = (url, dest) =>
 
      const page = await browser.newPage();
 
-  
+   
      let num = 0;
-     for (let i = 1; i < 26; i++) { /// u can change the range value of items list
+     for (let i = 1; i <= 25; i++) {
           if (num < i) {
                 num = i;
           }
-    
+        await page.setViewport({ width: 800, height: 600 });
         await page.goto(
-          `https://goddesskissove.flerogames.com/detail/character.html#${num}`
-        );
-        const SELECTOR_DEST =
-          'body > div > div.pic-chara > picture.pic-default.is-active > img';
+       `https://goddesskissove.flerogames.com/detail/character.html#${num}`
+     );
+     const SELECTOR_DEST =
+       'body > div > div.pic-chara > picture.pic-default.is-active > img';
 
-        let imageRef = await page.evaluate((qry) => {
-          return document
-            .querySelector(qry)
-            .getAttribute('src')
-            .replace('/', '');
-        }, SELECTOR_DEST);
-
-        let url_images = path.join(__dirname, `/images/myImages-${num}.png`);
-        await downloadFiles(imageRef, url_images); 
+     let imageRef = await page.evaluate((qry) => {
+       return document.querySelector(qry).getAttribute('src').replace('/', '');
+     }, SELECTOR_DEST);
+      
+     let  url_images =  path.join(__dirname, `/images/myImages-${num}.png`);
+      await downloadFiles(imageRef, url_images); 
 
    
      }
